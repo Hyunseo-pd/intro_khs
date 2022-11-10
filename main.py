0bdc7493575b3c51bacdca8ce542a9906b1f6efb
Hyunseo-pd <117896519+Hyunseo-pd@users.noreply.github.com>
2022-11-10 17:41:31 +0900

    main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-def on_button_pressed_a():
-    basic.show_number(input.temperature())
-    music.play_melody("C5 A B G A F G E ", 120)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+lightOn = False
 
 def on_sound_loud():
-    basic.show_leds("""
-        . . . . .
-                # . . . #
-                . . # . .
-                . # . # .
-                . . # . .
-    """)
+    global lightOn
+    lightOn = not (lightOn)
+    if lightOn:
+        basic.show_leds("""
+            . . . . .
+                        . # # # .
+                        . # # # .
+                        . # # # .
+                        . . . . .
+        """)
+    else:
+        basic.clear_screen()
 input.on_sound(DetectedSound.LOUD, on_sound_loud)
-
-def on_button_pressed_b():
-    music.play_tone(294, music.beat(BeatFraction.QUARTER))
-    basic.show_leds("""
-        . # # . .
-                . # . # .
-                . # # . .
-                . # . # .
-                . # # . .
-    """)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_logo_touched():
-    music.play_tone(294, music.beat(BeatFraction.QUARTER))
-input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,29 +1,9 @@
-input.onButtonPressed(Button.A, function () {
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    basic.showNumber(input.temperature())
-})
+let lightOn = false
 input.onSound(DetectedSound.Loud, function () {
-    basic.showLeds(`
-        # # . # #
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    music.playTone(294, music.beat(BeatFraction.Quarter))
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # # . .
-        . # . # .
-        . # # . .
-        `)
-})
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    music.playTone(330, music.beat(BeatFraction.Quarter))
-})
-basic.forever(function () {
-	
+    lightOn = !(lightOn)
+    if (lightOn) {
+        basic.showString("" + (lightOn))
+    } else {
+        basic.showString("" + (lightOn))
+    }
 })
